c4896087bca4c437caa20c56709b330ad4b87f0b
Amr Bashir <amr.bashir2015@gmail.com>
2023-08-08 19:47:10 +0300

    fix tests

--- examples/winit.rs
+++ examples/winit.rs
@@ -55,7 +55,7 @@ fn main() {
     })
 }
 
-fn load_icon(path: &std::path::Path) -> tray_icon::icon::Icon {
+fn load_icon(path: &std::path::Path) -> tray_icon::Icon {
     let (icon_rgba, icon_width, icon_height) = {
         let image = image::open(path)
             .expect("Failed to open icon path")
@@ -64,6 +64,5 @@ fn load_icon(path: &std::path::Path) -> tray_icon::icon::Icon {
         let rgba = image.into_raw();
         (rgba, width, height)
     };
-    tray_icon::icon::Icon::from_rgba(icon_rgba, icon_width, icon_height)
-        .expect("Failed to open icon")
+    tray_icon::Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
 }

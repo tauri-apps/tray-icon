b491c9886619d3a26876476b078d99a0ae788918
Tony <68118705+Legend-Master@users.noreply.github.com>
2024-05-28 01:09:55 +0800

    fix(windows): tray icon rect scaled by dpi (#164)
    * Fix tray icon rect dpi on windows
    
    * Add change file
    
    * typo
    
    * Remove unused util code

---
+++ .changes/windows-rect-dpi.md
@@ -0,0 +1,5 @@
+---
+"tray-icon": "patch"
+---
+
+Fix tray icon rect scaled by dpi on Windows
--- src/platform_impl/windows/util.rs
+++ src/platform_impl/windows/util.rs
@@ -4,20 +4,9 @@
 
 use std::ops::{Deref, DerefMut};
 
-use once_cell::sync::Lazy;
-use windows_sys::{
-    core::HRESULT,
-    Win32::{
-        Foundation::{FARPROC, HWND, S_OK},
-        Graphics::Gdi::{
-            GetDC, GetDeviceCaps, MonitorFromWindow, HMONITOR, LOGPIXELSX, MONITOR_DEFAULTTONEAREST,
-        },
-        System::LibraryLoader::{GetProcAddress, LoadLibraryW},
-        UI::{
-            HiDpi::{MDT_EFFECTIVE_DPI, MONITOR_DPI_TYPE},
-            WindowsAndMessaging::{IsProcessDPIAware, ACCEL, WINDOW_LONG_PTR_INDEX},
-        },
-    },
+use windows_sys::Win32::{
+    Foundation::HWND,
+    UI::WindowsAndMessaging::{ACCEL, WINDOW_LONG_PTR_INDEX},
 };
 
 pub fn encode_wide<S: AsRef<std::ffi::OsStr>>(string: S) -> Vec<u16> {
@@ -70,81 +59,6 @@ pub fn get_instance_handle() -> windows_sys::Win32::Foundation::HMODULE {
     }
 
     unsafe { &__ImageBase as *const _ as _ }
-}
-
-fn get_function_impl(library: &str, function: &str) -> FARPROC {
-    let library = encode_wide(library);
-    assert_eq!(function.chars().last(), Some('\0'));
-
-    // Library names we will use are ASCII so we can use the A version to avoid string conversion.
-    let module = unsafe { LoadLibraryW(library.as_ptr()) };
-    if module == 0 {
-        return None;
-    }
-
-    unsafe { GetProcAddress(module, function.as_ptr()) }
-}
-
-macro_rules! get_function {
-    ($lib:expr, $func:ident) => {
-        crate::platform_impl::platform::util::get_function_impl(
-            $lib,
-            concat!(stringify!($func), '\0'),
-        )
-        .map(|f| unsafe { std::mem::transmute::<_, $func>(f) })
-    };
-}
-
-pub type GetDpiForMonitor = unsafe extern "system" fn(
-    hmonitor: HMONITOR,
-    dpi_type: MONITOR_DPI_TYPE,
-    dpi_x: *mut u32,
-    dpi_y: *mut u32,
-) -> HRESULT;
-
-static GET_DPI_FOR_MONITOR: Lazy<Option<GetDpiForMonitor>> =
-    Lazy::new(|| get_function!("shcore.dll", GetDpiForMonitor));
-
-pub const BASE_DPI: u32 = 96;
-pub fn dpi_to_scale_factor(dpi: u32) -> f64 {
-    dpi as f64 / BASE_DPI as f64
-}
-
-#[allow(non_snake_case)]
-pub unsafe fn hwnd_dpi(hwnd: HWND) -> u32 {
-    if let Some(GetDpiForMonitor) = *GET_DPI_FOR_MONITOR {
-        // We are on Windows 8.1 or later.
-        let monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
-        if monitor == 0 {
-            return BASE_DPI;
-        }
-
-        let mut dpi_x = 0;
-        let mut dpi_y = 0;
-        #[allow(clippy::unnecessary_cast)]
-        if GetDpiForMonitor(monitor, MDT_EFFECTIVE_DPI, &mut dpi_x, &mut dpi_y) == S_OK {
-            dpi_x as u32
-        } else {
-            BASE_DPI
-        }
-    } else {
-        let hdc = GetDC(hwnd);
-        if hdc == 0 {
-            return BASE_DPI;
-        }
-
-        // We are on Vista or later.
-        if IsProcessDPIAware() == 1 {
-            // If the process is DPI aware, then scaling must be handled by the application using
-            // this DPI value.
-            GetDeviceCaps(hdc, LOGPIXELSX as _) as u32
-        } else {
-            // If the process is DPI unaware, then scaling is performed by the OS; we thus return
-            // 96 (scale factor 1.0) to prevent the window from being re-scaled by both the
-            // application and the WM.
-            BASE_DPI
-        }
-    }
 }
 
 #[inline(always)]

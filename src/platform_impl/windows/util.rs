41cfca49fe837b921f0c97560a7fc4bfdf537de2
Tony <legendmastertony@gmail.com>
2025-10-20 15:16:00 +0800

    Remove unused `Accel` from muda

--- src/platform_impl/windows/util.rs
+++ src/platform_impl/windows/util.rs
@@ -2,46 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-use std::ops::{Deref, DerefMut};
-
-use windows_sys::Win32::{
-    Foundation::HWND,
-    UI::WindowsAndMessaging::{ACCEL, WINDOW_LONG_PTR_INDEX},
-};
+use windows_sys::Win32::{Foundation::HWND, UI::WindowsAndMessaging::WINDOW_LONG_PTR_INDEX};
 
 pub fn encode_wide<S: AsRef<std::ffi::OsStr>>(string: S) -> Vec<u16> {
     std::os::windows::prelude::OsStrExt::encode_wide(string.as_ref())
         .chain(std::iter::once(0))
         .collect()
-}
-
-/// ACCEL wrapper to implement Debug
-#[derive(Clone)]
-#[repr(transparent)]
-pub struct Accel(pub ACCEL);
-
-impl std::fmt::Debug for Accel {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("ACCEL")
-            .field("key", &self.0.key)
-            .field("cmd", &self.0.cmd)
-            .field("fVirt", &self.0.fVirt)
-            .finish()
-    }
-}
-
-impl Deref for Accel {
-    type Target = ACCEL;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for Accel {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
 }
 
 // taken from winit's code base

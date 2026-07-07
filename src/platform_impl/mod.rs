9e4cc0b5babfa6722b15f6ca41623ce91b023cd3
Anthony Smith <wombatvagina69@gmail.com>
2026-07-07 15:12:07 -0400

    feat(linux): add ksni tray backend

--- src/platform_impl/mod.rs
+++ src/platform_impl/mod.rs
@@ -6,13 +6,18 @@
 #[path = "windows/mod.rs"]
 mod platform;
 #[cfg(any(
-    target_os = "linux",
     target_os = "dragonfly",
     target_os = "freebsd",
     target_os = "netbsd",
     target_os = "openbsd"
 ))]
 #[path = "gtk/mod.rs"]
+mod platform;
+#[cfg(all(target_os = "linux", not(feature = "linux-ksni")))]
+#[path = "gtk/mod.rs"]
+mod platform;
+#[cfg(all(target_os = "linux", feature = "linux-ksni"))]
+#[path = "linux/mod.rs"]
 mod platform;
 #[cfg(target_os = "macos")]
 #[path = "macos/mod.rs"]
